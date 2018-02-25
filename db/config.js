3fcd89852c956149d1c1c35863459130b041f4e6
TJ Stubbs <stubbsrule@gmail.com>
2018-02-25 12:45:26 -0500

    create db and config.js

---
+++ db/config.js
@@ -0,0 +1,25 @@
+const options = {
+  receive: (data, result, e) => {
+
+  },
+  query: (e) => {
+
+  }
+}
+const pgp = require('pg-promise')(options);
+
+function setDatabase(){
+  if(process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+    return pgp({
+      database: 'new_blog_app',
+      port: 5432,
+      host:'localhost'
+    });
+  } else if (process.env.NODE_ENV === 'production') {
+    return pgp(process.env.DATABASE_URL);
+  }
+}
+
+const db = setDatabase();
+
+module.exports = db;
---
+++ db/seeds/seed.sql
@@ -0,0 +1,3 @@
+\c new_blog_app;
+
+INSERT INTO posts(title, content) VALUES ('Post one', 'This is the content for post one.');
